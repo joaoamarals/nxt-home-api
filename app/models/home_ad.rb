229d84898f1f4f6dd6403d3315326778bb69c11a
João Amaral Santos <joaoamsantos@deloitte.pt>
2023-12-03 20:11:50 +0100

    Create methods to fetch specs from an home ad

--- app/models/home_ad.rb
+++ app/models/home_ad.rb
@@ -1,3 +1,24 @@
+# frozen_string_literal: true
+
 class HomeAd < ApplicationRecord
   serialize :elements, Hash
+
+  after_commit do
+    ActiveSupport::Notifications.instrument(
+      'created.home_ad', { home_ad: self }
+    )
+  end
+
+  def location
+    elements[:link].split('/')[2]
+  end
+
+  def no_of_rooms
+    elements[:features]['number-of-rooms'].match(/[0-9]/)[0]
+  end
+
+  def price
+    elements[:price].match(/[0-9]+/)[0].to_i
+  end
+
 end
