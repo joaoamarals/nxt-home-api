5755b7cdd10586816cd0ae8d1aab1b48d5c6bc3f
João Amaral Santos <joaoamsantos@deloitte.pt>
2023-12-05 00:15:26 +0100

    Add the subscribe to create ad home events

--- config/initializers/event_handlers.rb
+++ config/initializers/event_handlers.rb
@@ -1,4 +1,7 @@
-#
-# ActiveSupport::Notifications.subscribe 'created.home_ad' do |event|
-#   HomeAd::NotifyUsersJob.perform_later(event.payload[:home_ad])
-# end
+Rails.application.config.after_initialize do
+  ActiveSupport::Notifications.subscribe 'created.home_ad' do |event|
+    puts 'Sending email to users...'
+    puts event.payload[:home_ad]
+    NotifyNewMatchJob.perform_later(event.payload[:home_ad])
+  end
+end
