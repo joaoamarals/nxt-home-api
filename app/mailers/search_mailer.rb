534c0cf3ebd4896972a790bb56084aebe4972ba1
João Amaral Santos <joaoamsantos@deloitte.pt>
2023-12-08 10:37:49 +0100

    Change to send the notification email to the email related to the search

--- app/mailers/search_mailer.rb
+++ app/mailers/search_mailer.rb
@@ -2,6 +2,6 @@ class SearchMailer < ApplicationMailer
   def new_match
     search = params[:search]
     @home_ad = params[:home_ad]
-    mail(to: 'j.amaral.s@gmail.com', bcc: 'j.amaral.s@gmail.com', subject: 'New match!')
+    mail(to: search.user.email, bcc: 'j.amaral.s@gmail.com', subject: 'New match!')
   end
 end
